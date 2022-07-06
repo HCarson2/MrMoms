eda05b20bd7a918228c91d47a336408d80e18797
HCarson2 <mair0610@gmail.com>
2022-07-05 23:16:23 -0400

    third commit

--- backend_server/server.js
+++ backend_server/server.js
@@ -3,12 +3,18 @@ require('dotenv').config()
 
 // IMPORT EXPRESS IN ORDER TO CREATE OUR EXPRESS APPLICATION
 const express = require('express');
+
+// IMPORT MONGOOSE IN ORDER TO CONNECT TO OUR DATABASE
+const mongoose = require("mongoose");
+
 const mrMomRoutes = require('./routes/mrMoms')
 
 // CREATE A VARIABLE TO HOLD OUR EXPRESS METHODS
 const app = express();
 
-// Middleware
+// Middleware|| Looking for body
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
@@ -16,6 +22,24 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/mrMoms', mrMomRoutes)
+
+const DB = mongoose
+  .connect(
+    process.env.DATABASE.replace("<password>", process.env.PASSWORD),
+    // {
+    //   useCreateIndex: true,
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useFindAndModify: false,
+    // }
+  )
+  .then(() => {
+    console.log("DATABASE IS UP AND RUNNING...");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
 
 // Listen for request
 app.listen(process.env.PORT, () => {
